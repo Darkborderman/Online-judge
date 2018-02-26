c051c14f0bccfd9f58e60822e34283df4425ed05
Darkborderman <reastw1234@gmail.com>
2018-02-26 15:13:30 +0800

    ==Add UVa 441==

--- 441.cpp
+++ 441.cpp
@@ -1,40 +1,49 @@
 #include<iostream>
 
 using namespace std;
-void backtrack(int,int);
-int store[6];
-int input;
-int space[13];
-bool flag[13]={0};
+
+int length=0,choice[13]={0},option=0,answer[6],ischoose[13]={0};
+bool isfirst=true;
+
+int recur(int x,int i);
+
 int main()
 {
-	int counter=0;
-	while(cin>>input&&input)
-	{
-		if(!counter) counter++;
-		else cout<<endl;
-		for(int i=0;i<=input-1;i++)
-		{
-			cin>>space[i];
-		}
-		backtrack(0,0);
-	}
-	return 0;
+    while(cin>>option)
+    {
+        if(option==0) return 0;
+        if(isfirst)
+        {
+            isfirst=false;
+        }
+        else{ cout<<endl;}
+        for(int i=0;i<=option-1;i++)
+        {
+            int num;
+            cin>>num;
+            choice[i]=num;
+        }
+        recur(0,0);
+    }
+    return 0;
 }
-void backtrack(int x,int cur)
+
+int recur(int nest,int th)
 {
-	if(x>=6)
-	{
-		cout<<store[0]<<" "<<store[1]<<" "<<store[2]<<" "<<store[3]<<" "<<store[4]<<" "<<store[5]<<endl;
-		return;
-	}
-	else	if(cur==input) return;
-	else
-	{
-		for(int i=cur;i<=input-1;i++)
-		{
-			store[x]=space[i];
-			backtrack(x+1,i+1);
-		}
-	}
+    if(nest==6)
+    {
+        for(int i=0;i<=4;i++) cout<<answer[i]<<" ";
+        cout<<answer[5]<<endl;
+        return 0;
+    }
+    for(int i=th;i<=option-1;i++)
+    {
+        if(!ischoose[i])
+        {
+            ischoose[i]=1;
+            answer[nest]=choice[i];
+            recur(nest+1,i+1);
+            ischoose[i]=0;
+        }
+    }
 }
